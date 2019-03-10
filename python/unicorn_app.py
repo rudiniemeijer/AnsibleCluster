564aad7b8657a1da1333bf1e9b6bddbddffdc2a7
Rudi Niemeijer <rudiniemeijer@gmail.com>
2019-03-10 18:17:24 +0100

    Update unicorn_app.py

--- python/unicorn_app.py
+++ python/unicorn_app.py
@@ -24,15 +24,12 @@ def showpixel():
   x = request.args.get('x', default=None, type=int)
   y = request.args.get('y', default=None, type=int)
   hue = request.args.get('hue', default=None, type=float)
-  r = request.args.get('r', default=None, type=int)
-  g = request.args.get('g', default=None, type=int)
-  b = request.args.get('b', default=None, type=int)
-  
 
-  if sensor_value is not None:
-    data_set = {'value':sensor_value}
-    return str(data_set), 200
+  if (x is not None) and (y is not None) and (hue is not None):
+    uh.set_pixel(x,y,to_rgb(hue))
+    uh.show()
+    return "", 200
   else:
-    return "{'usage':'http://%s/showpixel?x=[0..15]&y=[0..3]&sensor=temperature|pressure|color|light|analog|compass|accelerometer'}" % hostname, 400
+    return "{'usage':'http://%s/showpixel?x=[0..15]&y=[0..3]&hue=[0..1]'}" % hostname, 400
 
 app.run(debug=False, host=hostname, port=4000)
