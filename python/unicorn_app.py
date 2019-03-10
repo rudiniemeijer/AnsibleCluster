d960f8bc6518874894d40e512c34372d34450133
Rudi Niemeijer <rudiniemeijer@gmail.com>
2019-03-10 21:09:47 +0100

    Update unicorn_app.py

--- python/unicorn_app.py
+++ python/unicorn_app.py
@@ -1,64 +1,91 @@
 # Prepare for use with:
-# pip install unicornhat
+# sudo pip install unicornhat
 
-# run this script with python unicorn_app.py
+# run this script with sudo python unicorn_app.py
 
 # import the Pimoroni Unicorn library
 import unicornhat as uh
 import time, socket # Used to retrieve hostname
 from flask import Flask, request # Flask microservice
+from random import choice, randint
 
 # Specify that the pHAT is used (instead of a HAT)
 uh.set_layout(uh.PHAT)
 
-# Most important commands
-# .brightness(0..1)
-# .set_pixel(x=0..15, y=0..3, r, g, b)
-# .show()
+max_x, max_y = uh.get_shape()
 
 app = Flask(__name__)
 hostname = socket.gethostname()
 
+# Calculate approximate RGB values from hue parameter
 def to_rgb(hue):
-  rgb = {r=0, g=0, b=0}
-  
-  if hue < (1 / 3):
-    rgb.r = 2 - hue * 6
-    rgb.g = hue * 6
-    rgb.b = 0
-  elif hue < (2 / 3):
-    rgb.r = 0
-    rgb.g = hue * 6
-    rgb.b = hue * 6 - 2
-  else
-    rgb.r = hue * 6 - 4
-    rgb.g = 0
-    rgb.b = (1 - hue) * 6
-  
-  if rgb.r > 1:
-    rgb.r = 1
-  if rgb.g > 1:
-    rgb.g = 1
-  if rgb.b > 1:
-    rgb.b = 1
-    
-  rgb.r = rgb.r * 255
-  rgb.g = rgb.g * 255
-  rgb.b = rgb.b * 255
-  
-  return rgb
+  if hue < (1.0 / 3.0):
+    r = 2 - hue * 6
+    g = hue * 6
+    b = 0
+  elif hue < (2.0 / 3.0):
+    r = 0
+    g = 4 - hue * 6
+    b = hue * 6 - 2
+  else:
+    r = hue * 6 - 4
+    g = 0
+    b = (1 - hue) * 6
 
-@app.route('/showpixel')
-def showpixel():
+  if r > 1:
+    r = 1
+  if g > 1:
+    g = 1
+  if b > 1:
+    b = 1
+
+  r = int(r * 255)
+  g = int(g * 255)
+  b = int(b * 255)
+
+  return (r, g, b)
+
+# Determine if the pixel is in an acceptable range
+def valid_pixel(x, y, hue):
+  if x < 0 or x >= max_x or y < 0 or y >= max_y or hue < 0 or hue > 1:
+    return False
+  else:
+    return True 
+
+@app.route('/set')
+def set():
   x = request.args.get('x', default=None, type=int)
   y = request.args.get('y', default=None, type=int)
   hue = request.args.get('hue', default=None, type=float)
 
+  invalid_msg = "{'usage':'http://%s/set?x=[0..%s]&y=[0..%s]&hue=[0..1]'}" % (hostname, max_x - 1, max_y - 1)
+
   if (x is not None) and (y is not None) and (hue is not None):
-    uh.set_pixel(x,y,to_rgb(hue))
-    uh.show()
-    return "", 200
+    if valid_pixel(x,y,hue):
+      r,g,b = to_rgb(hue)
+      uh.set_pixel(x,y,r,g,b)
+      uh.show()
+      return "{'result':'Set a pixel at (%s,%s)'}" % (x,y), 200
+    else:
+      return invalid_msg, 406
   else:
-    return "{'usage':'http://%s/showpixel?x=[0..15]&y=[0..3]&hue=[0..1]'}" % hostname, 400
+    return invalid_msg, 400
+
+@app.route('/any')
+def any():
+  x = randint(0, max_x - 1)
+  y = randint(0, max_y - 1)
+  r = choice(0, 128, 255)
+  g = choice(0, 128, 255)
+  b = choice(0, 128, 255)
+  uh.set_pixel(x,y,r,g,b)
+  uh.show()
+  return "{'result':'Set a random pixel'}", 200
+
+@app.route('/clear')
+def clear():
+  uh.clear()
+  uh.show()
+  return "{'result':'Cleared the display'}", 200
 
 app.run(debug=False, host=hostname, port=4000)
