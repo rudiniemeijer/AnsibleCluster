0f68982848656aefa8efe950d3ab8cccf8f498ba
Rudi Niemeijer <rudiniemeijer@gmail.com>
2019-03-10 17:26:25 +0100

    Update unicorn_app.py

--- python/unicorn_app.py
+++ python/unicorn_app.py
@@ -5,7 +5,7 @@
 
 # import the Pimoroni Unicorn library
 import unicornhat as uh
-import time, os
+import time, socket # Used to retrieve hostname
 from flask import Flask, request # Flask microservice
 
 # Specify that the pHAT is used (instead of a HAT)
@@ -19,35 +19,20 @@ uh.set_layout(uh.PHAT)
 app = Flask(__name__)
 hostname = socket.gethostname()
 
-@app.route('/measure')
-def measure():
-  leds.on()
-  sensor = request.args.get('sensor', default=None, type=str)
-  sensor_value = None
-  if sensor == 'temperature':
-    sensor_value = weather.temperature()
-  elif sensor == 'pressure':
-    sensor_value = weather.pressure('hPa')
-  elif sensor == 'color':
-    sensor_value = light.rgb()
-  elif sensor == 'light':
-    leds.off()
-    sensor_value = light.light()
-    leds.on()
-  elif sensor == "analog":
-    sensor_value = analog.read_all()
-  elif sensor == 'compass':
-    sensor_value = motion.heading()
-  elif sensor == 'accelerometer':
-    sensor_value = motion.accelerometer()
-  else:
-    sensor_value = None
-  leds.off()
+@app.route('/showpixel')
+def showpixel():
+  x = request.args.get('x', default=None, type=int)
+  y = request.args.get('y', default=None, type=int)
+  hue = request.args.get('hue', default=None, type=float)
+  r = request.args.get('r', default=None, type=int)
+  g = request.args.get('g', default=None, type=int)
+  b = request.args.get('b', default=None, type=int)
+  
 
   if sensor_value is not None:
     data_set = {'value':sensor_value}
     return str(data_set), 200
   else:
-    return "{'usage':'http://%s/measure?sensor=temperature|pressure|color|light|analog|compass|accelerometer'}" % hostname, 400
+    return "{'usage':'http://%s/showpixel?x=[0..15]&y=[0..3]&sensor=temperature|pressure|color|light|analog|compass|accelerometer'}" % hostname, 400
 
 app.run(debug=False, host=hostname, port=4000)
