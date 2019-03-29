5d163b2fce256ebb17eeceb7f07ef2042c5a2ac1
Evert Mulder <evert.mulder@kpn.com>
2019-03-29 15:31:42 +0100

    Update script for updating ansible-controller

---
+++ voorbereiding/roles/ansible_server/files/cunobow.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+url=p24.local
+
+curl -s "http://$url:4000/clear" > /dev/null
+curl -s "http://$url:4000/set?x=0&y=0&hue=0.05" > /dev/null
+curl -s "http://$url:4000/set?x=1&y=0&hue=0.08" > /dev/null
+curl -s "http://$url:4000/set?x=2&y=0&hue=0.10" > /dev/null
+curl -s "http://$url:4000/set?x=3&y=0&hue=0.13" > /dev/null
+curl -s "http://$url:4000/set?x=4&y=0&hue=0.16" > /dev/null
+curl -s "http://$url:4000/set?x=5&y=0&hue=0.18" > /dev/null
+curl -s "http://$url:4000/set?x=6&y=0&hue=0.21" > /dev/null
+curl -s "http://$url:4000/set?x=7&y=0&hue=0.24" > /dev/null
+curl -s "http://$url:4000/set?x=0&y=1&hue=0.27" > /dev/null
+curl -s "http://$url:4000/set?x=1&y=1&hue=0.29" > /dev/null
+curl -s "http://$url:4000/set?x=2&y=1&hue=0.32" > /dev/null
+curl -s "http://$url:4000/set?x=3&y=1&hue=0.35" > /dev/null
+curl -s "http://$url:4000/set?x=4&y=1&hue=0.37" > /dev/null
+curl -s "http://$url:4000/set?x=5&y=1&hue=0.40" > /dev/null
+curl -s "http://$url:4000/set?x=6&y=1&hue=0.43" > /dev/null
+curl -s "http://$url:4000/set?x=7&y=1&hue=0.46" > /dev/null
+curl -s "http://$url:4000/set?x=0&y=2&hue=0.48" > /dev/null
+curl -s "http://$url:4000/set?x=1&y=2&hue=0.51" > /dev/null
+curl -s "http://$url:4000/set?x=2&y=2&hue=0.54" > /dev/null
+curl -s "http://$url:4000/set?x=3&y=2&hue=0.56" > /dev/null
+curl -s "http://$url:4000/set?x=4&y=2&hue=0.59" > /dev/null
+curl -s "http://$url:4000/set?x=5&y=2&hue=0.62" > /dev/null
+curl -s "http://$url:4000/set?x=6&y=2&hue=0.64" > /dev/null
+curl -s "http://$url:4000/set?x=7&y=2&hue=0.67" > /dev/null
+curl -s "http://$url:4000/set?x=0&y=3&hue=0.70" > /dev/null
+curl -s "http://$url:4000/set?x=1&y=3&hue=0.73" > /dev/null
+curl -s "http://$url:4000/set?x=2&y=3&hue=0.75" > /dev/null
+curl -s "http://$url:4000/set?x=3&y=3&hue=0.78" > /dev/null
+curl -s "http://$url:4000/set?x=4&y=3&hue=0.81" > /dev/null
+curl -s "http://$url:4000/set?x=5&y=3&hue=0.83" > /dev/null
+curl -s "http://$url:4000/set?x=6&y=3&hue=0.86" > /dev/null
+curl -s "http://$url:4000/set?x=7&y=3&hue=0.89" > /dev/null
